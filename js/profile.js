a63bb13033ff9df8a8ee08dfc62187567825ff5b
lee-yun-pyo <41375597+lee-yun-pyo@users.noreply.github.com>
2021-04-11 23:55:02 +0900

    revise mainProf-bars_bar

--- js/profile.js
+++
@@ -1,8 +0,0 @@
-const list = document.querySelector(".mainProf-bars_bar");
-
-console.log(list);
-
-function colorChange() {
-    list.style.color = "red";
-}
-list.addEventListener("click", colorChange);
